7ad9d640fd6f3da9718167828c2a06050f93c371
souzakayque <52177811+pereirakayque@users.noreply.github.com>
2021-07-20 12:51:21 -0300

    creating feedback controller and command handler

---
+++ Application/Handlers/Commands/RegisterFeedbackCommand.cs
@@ -0,0 +1,56 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Responses;
+using Application.Interfaces.Application;
+using FluentValidation;
+using MediatR;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Commands
+{
+    public class RegisterFeedbackCommand : IRequest<ResponseModel>
+    {
+        [Required]
+        public string Message { get; set; }
+
+        [JsonIgnore]
+        public string Token { get; set; }
+    }
+
+    public class FeedbackCommandValidator : AbstractValidator<RegisterFeedbackCommand>
+    {
+        public FeedbackCommandValidator()
+        {
+            RuleFor(c => c.Message).NotEmpty();
+            RuleFor(c => c.Token).NotEmpty();
+        }
+    }
+
+    public class FeedbackCommandHandler : IRequestHandler<RegisterFeedbackCommand, ResponseModel>
+    {
+        private readonly ISystemUserActions systemUser;
+        public FeedbackCommandHandler(ISystemUserActions systemUser)
+        {
+            this.systemUser = systemUser;
+        }
+
+        public async Task<ResponseModel> Handle(RegisterFeedbackCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                if (request != null)
+                {
+
+                }
+                return ResponseModel.Failure("Request is empty... Feedback registration failed.");
+            }
+            catch (Exception ex) 
+            { 
+                throw new CustomException($"An error occured with description -> {ex.Message}"); 
+            }
+        }
+    }
+}
---
+++ HeartInsurance.API/Controllers/Version1/FeedbackController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Responses;
+using Application.Handlers.Commands;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace HeartInsurance.API.Controllers
+{
+    [Route("feedback")]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class FeedbackController : BaseEntryController
+    {
+
+        /// <summary>
+        /// The endpoint for user register a feedback.
+        /// </summary>
+        /// <param name="command">The request payload</param>
+        /// <returns>Expected to return success if required payload entered.</returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseModel), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ResponseModel), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(ResponseModel), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> Post([FromHeader] string token, [FromBody] RegisterFeedbackCommand command)
+        {
+            command.Token = token;
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
