caaab186372cbca06a2c34abe9286e769399d111
Kenneth Otoro <codexkenth@outlook.com>
2021-08-06 18:00:04 +0100

    [updated ] -> Added profile page in FE.

---
+++ heart-web-insurance/HeartInsurance.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HeartInsurance.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+        public IActionResult Users() => View();
+        public IActionResult UsersDetails() => View();
+    }
+}
